3482729fbac61466b3fb6b777e8ce952ae28b5b3
tharunnemaliga <nemaligatharun@hotmail.com>
2024-07-06 16:41:51 +0200

    Copyspecial updated solution
    Copy Special exercise

--- google-python-exercises/copyspecial/copyspecial.py
+++ google-python-exercises/copyspecial/copyspecial.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
 #!/usr/bin/python3
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
@@ -6,50 +12,65 @@
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
-import sys
-import re
 import os
+import re
 import shutil
 import subprocess
+import sys
 
 """Copy Special exercise
 """
 
-# +++your code here+++
-# Write functions and modify main() to call them
+
+# LAB(begin solution)
+def get_special_paths(dirname):
+    """Given a dirname, returns a list of all its special files."""
+    result = []
+    paths = os.listdir(dirname)  # list of paths in that dir
+    for fname in paths:
+        match = re.search(r'__(\w+)__', fname)
+        if match:
+            result.append(os.path.abspath(os.path.join(dirname, fname)))
+    return result
 
 
+def copy_to(paths, to_dir):
+    """Copy all of the given files to the given dir, creating it if necessary."""
+    if not os.path.exists(to_dir):
+        os.mkdir(to_dir)
+    for path in paths:
+        fname = os.path.basename(path)
+        shutil.copy(path, os.path.join(to_dir, fname))
 
-def main():
-  # This basic command line argument parsing code is provided.
-  # Add code to call your functions below.
 
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
-  args = sys.argv[1:]
-  if not args:
-    print('usage: [--todir dir][--tozip zipfile] dir [dir ...]')
-    sys.exit(1)
+def zip_to(paths, zipfile):
+    """Zip up all of the given files into a new zip file with the given name."""
+    cmd = 'zip -j ' + zipfile + ' ' + ' '.join(paths)
+    print("Command I'm going to do:" + cmd)
+    (status, output) = subprocess.getstatusoutput(cmd)
+    if status:
+        sys.stderr.write(output)
 
-  # todir and tozip are either set from command line
-  # or left as the empty string.
-  # The args array is left just containing the dirs.
-  todir = ''
-  if args[0] == '--todir':
-    todir = args[1]
-    del args[0:2]
+# LAB(end solution)
 
-  tozip = ''
-  if args[0] == '--tozip':
-    tozip = args[1]
-    del args[0:2]
+def process_special_files(dirs, todir='', tozip=''):
+    # Gather all the special files
+    paths = []
+    for dirname in dirs:
+        paths.extend(get_special_paths(dirname))
 
-  if not args: # A zero length array evaluates to "False".
-    print('error: must specify one or more dirs')
-    sys.exit(1)
+    if todir:
+        copy_to(paths, todir)
+    elif tozip:
+        zip_to(paths, tozip)
+    else:
+        return paths
 
-  # +++your code here+++
-  # Call your functions
 
-if __name__ == '__main__':
-  main()
+dirs = [r'D:\Isha\ME\studies\assignments']
+todir = ''
+tozip = ''
+
+special_files = process_special_files(dirs, todir, tozip)
+for file in special_files:
+    print(file)
